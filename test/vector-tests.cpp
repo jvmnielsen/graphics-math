dcdc2eab5301ab9d822890f861bf7271de28d107
Jacob Vesti Moeslund Nielsen <jvmnielsen@gmail.com>
2020-02-12 19:45:34 +0100

    Expanded length tests

--- test/vector-tests.cpp
+++ test/vector-tests.cpp
@@ -1,9 +1,8 @@
 #include <catch2/catch.hpp>
 #include <graphics-math.hpp>
-#include <gcem.hpp>
 
 TEMPLATE_TEST_CASE( 
-    "Vec3s with integers have length", "[Vec3]", 
+    "Vec3s (integers) have length", "[Vec3]", 
     std::int32_t, std::int64_t) 
 {
     auto v = gm::Vec3<TestType>{ 1, 2, 6 };
@@ -13,39 +12,40 @@ TEMPLATE_TEST_CASE(
 
     REQUIRE( v.length() == 6 );
 
-    SECTION( "length is available at compile time" ) {
-        auto constexpr u = gm::Vec3<TestType>{ 1, 2, 6 };
-        if constexpr (u.length() == 6) {
-            REQUIRE( true );
-        } else {
-            REQUIRE( false );
-        }
-    }
-
     SECTION( "length changes with resize" ) {
         v.x = 63;
         REQUIRE( v.length() == 63 );
     }
+
+    SECTION( "default length is zero" ) {
+        auto const w = gm::Vec3<TestType>{ };
+        REQUIRE( w.length() == 0 );
+    }
 }
 
 TEMPLATE_TEST_CASE( 
-    "Vec3s with floats have length", "[Vec3]", 
+    "Vec3s (floating point) have length", "[Vec3]", 
     float, double) 
 {
-    auto constexpr v = gm::Vec3<TestType>{ 1, 2, 6 };
+    auto v = gm::Vec3<TestType>{ 1, 2, 6 };
     REQUIRE( v.x == 1 );
     REQUIRE( v.y == 2 );
     REQUIRE( v.z == 6 );
 
-    REQUIRE( std::abs(v.length() - 6.403) <= 0.001 );
+    REQUIRE( v.length() == Approx(6.403).epsilon(0.01) );
 
-    SECTION( "length is available at compile time" ) {
-        if constexpr (gcem::abs(v.length() - 6.403) <= 0.001) {
-            REQUIRE( true );
-        } else {
-            REQUIRE( false );
-        }
+    SECTION( "length changes with resize" ) {
+        v.x = 63;
+        REQUIRE( v.length() == Approx(63.317).epsilon(0.01) );
     }
 
+    SECTION( "negative length is the same" ) {
+        v.x = -63;
+        REQUIRE( v.length() == Approx(63.317).epsilon(0.01) );
+    }
 
+    SECTION( "default length is zero" ) {
+        auto const w = gm::Vec3<TestType>{ };
+        REQUIRE( w.length() == Approx(0).epsilon(0.001) );
+    }
 }
