0d267ce025e705b3023271c7fadcf8e2393ec3d9
Jacob Vesti Moeslund Nielsen <jvmnielsen@gmail.com>
2020-02-12 16:47:43 +0100

    Basic setup for Catch2 and testing

---
+++ test/vector-test.cpp
@@ -0,0 +1,20 @@
+#include <catch2/catch.hpp>
+
+SCENARIO( "vectors can be sized and resized", "[vector]" ) {
+
+    GIVEN( "A vector with some items" ) {
+        std::vector<int> v( 5 );
+
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 5 );
+
+        WHEN( "the size is increased" ) {
+            v.resize( 10 );
+
+            THEN( "the size and capacity change" ) {
+                REQUIRE( v.size() == 10 );
+                REQUIRE( v.capacity() >= 10 );
+            }
+        }
+    }
+}
